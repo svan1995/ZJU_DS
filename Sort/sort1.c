62f20d0a777760087b8d8f17876b0a32b9f4c80f
svan666 <18868129300@163,com>
2018-05-04 19:05:34 +0800

    add insert_sort

--- Sort/sort1.c
+++ Sort/sort1.c
@@ -3,7 +3,7 @@ void BubbleSort(ElementType A[], int N)
 	int P, i;
 	bool flag;
 
-	for(P = N-1, P >= 0, P--){
+	for(P = N-1; P >= 0; P--){
 		flag = false;
 		for(i = 0；i < P; i++){
 			if(A[i] > A[i+1]){
@@ -15,3 +15,17 @@ void BubbleSort(ElementType A[], int N)
 			break;
 	}
 }
+
+void InsertionSort(ElementType A[], int N)
+{
+	int P, i;
+	ElementType Tmp;
+
+	for(P = 1; P < N; P++){
+		Tmp = A[p];
+		for (int i = P; i > 0 && A[i-1] > Tmp; i--)
+			A[i] = A[i-1];
+		A[i] = Tmp;
+	}
+}
+
