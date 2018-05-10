c8e8a3f0c4f6ee21f795e0402139adf43cfa6855
svan666 <18868129300@163,com>
2018-05-10 16:06:24 +0800

    add MSDsort

--- Sort/sort3.c
+++ Sort/sort3.c
@@ -73,3 +73,55 @@ void LSDRadixSort(ElementType A[], int N)
 		free(tmp);
 	}
 }
+
+void MSD(ElementType A[], int L, int R, int D)
+{
+	int Di, i, j;
+	Bucket B;
+	PtrToNode tmp, p, List = NULL;
+
+	if(D == 0)
+		return;
+
+	for(i = 0; i < Radix, i++ )
+		B[i].head = B[i].tail = NULL;
+	for(i = L; i <= R, i++ ){
+		tmp = (PtrToNode)malloc(sizeof(struct Node));
+		tmp->Key = A[i];
+		tmp->next = NULL;
+		List = tmp;
+	}
+
+	p = List;
+	while(p){
+		Di = GetDigit(p->Key, D);
+		tmp = p;
+		p = p->next;
+		if(B[Di].head == NULL)
+			B[Di].tail = tmp;
+		tmp->next = B[Di].head;
+		B[Di].head = tmp;
+	}
+
+	i = j = L;
+	for(Di = 0; Di < Radix; Di++){
+		if(B[Di].head){
+			p = B[Di].head;
+			while(p){
+				tmp = p;
+				p = p->next;
+				A[j++] = tmp->Key;
+				free(tmp);
+			}
+			MSD(A,i,j-1,D-1);
+			i = j;
+		}
+	}
+	
+}
+
+void MSDRadixSort(ElementType A[], int N)
+{
+	MSD(A, 0, N-1, MaxDigit);
+}
+
